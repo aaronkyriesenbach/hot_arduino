d4abe51ed65b1133fadb111b41a27683b2398db6
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2021-11-12 11:42:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Arduino-powered hot potato game
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+void setup() {
+// write your initialization code here
+}
+
+void loop() {
+    // int num_players = 2;
+    // if (digitalRead(UP_BUTTON)) { num_players++; }
+    // else if (digitalRead(DOWN_BUTTON)) { num_players--; }
+    // if (digitalRead(START_BUTTON)) { play_game(num_players); }
+}
+
+/*
+ * void play_game(int num_players) {
+ *     int millis_to_next_event = rand() * 10 + 10;
+ *     while (num_players > 1) {
+ *         play_music();
+ *         if (millis_to_next_event == millis()) {
+ *             events[rand() * 4](&num_players);
+ *             millis_to_next_event = 0;
+ *         }
+ *     }
+ * }
+ */
